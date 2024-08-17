d126c8a47d37b0024a1cf522dc257decd909f466
Hishock <osmar.ancco@tecsup.edu.pe>
2024-08-16 19:00:49 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/main.kt
@@ -0,0 +1,25 @@
+package acuario
+
+fun construirAcuario() {
+
+    val acuario6 = Acuario(numeroDePeces = 29)
+    acuario6.imprimirTamano()
+
+    val acuario7 = Acuario(numeroDePeces = 29)
+    acuario7.imprimirTamano()
+    acuario7.volumen = 70
+    acuario7.imprimirTamano()
+
+    val acuario8 = Acuario(largo = 25, ancho = 25, alto = 40)
+    acuario8.imprimirTamano()
+
+    val miAcuario = Acuario(ancho = 25, largo = 25, alto = 40)
+    miAcuario.imprimirTamano()
+
+    val miTorre = TanqueTorre(diametro = 25, alto = 40)
+    miTorre.imprimirTamano()
+}
+
+fun main() {
+    construirAcuario()
+}
